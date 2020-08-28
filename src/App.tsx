bc517d228bfdfd79b62d4f9496a4ec77bf5f1ea2
Matthew Crompton <crompton.ml@gmail.com>
2020-08-28 18:16:21 +0200

    Add disableCheck

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
-import React, { useState } from "react";
 import data from "./food_data/fooddata.json";
+import React, { useState } from "react";
 import { CATEGORIES } from "./food_data/categories";
 
 type Category =
@@ -10,40 +10,123 @@ type Category =
   | "SNACKSIDE"
   | "BREAKFAST";
 
-// type Price = "$" | "$$" | "$$$";
-// interface FoodItem {
-//   ITEM: string;
-//   CAL: string;
-//   PRICE: string;
-//   CATEGORY: Category;
-//   ID: string;
-// }
+interface FoodItem {
+  ITEM: string;
+  CAL: string;
+  CATEGORY: string;
+  PRICE: string;
+  ID: string;
+}
 
-const fooddata = data;
+interface User {
+  id: string;
+  favList: string[];
+  name: string;
+}
+
+interface CalendarItem {
+  id: string;
+  date: string;
+  quantity: number;
+  user: string;
+}
 
 type ButtonProps = {
   callback: (name: string) => void;
 };
 
 const foodList: string[] = CATEGORIES;
+const fooddata: FoodItem[] = data;
 
 function App() {
-  // const handleTypeClick = (name: string) => {
-  //   console.log(name);
-  // };
-  const [itemListToday, setItemListToday] = useState([]);
+  const [calendar, setCalendar] = useState<CalendarItem[]>([]);
+  const [userList, setUserList] = useState<User[]>([
+    {
+      id: "99",
+      favList: ["1", "2"],
+      name: "Ugly Child",
+    },
+    {
+      id: "77",
+      favList: ["3", "21"],
+      name: "Stinko",
+    },
+  ]);
+  const [user, setUser] = useState<User>({
+    id: "99",
+    favList: ["1", "2"],
+    name: "Ugly Child",
+  });
+
+  const findCalendarItem = (item: CalendarItem, id: string) =>
+    item.date === "today" && item.user === user.id && item.id === id;
+
+  const disableCheck = (id: string): boolean => {
+    const filtedItem = calendar.filter((item) => item.id === id);
+    return !filtedItem[0];
+  };
+
+  const addItem = (id: string, amount: number) => {
+    const newCalendarItem: CalendarItem = {
+      id,
+      date: "today",
+      quantity: amount,
+      user: user.id,
+    };
+    setCalendar((prevState) => [...prevState, newCalendarItem]);
+  };
+
+  const removeItem = (id: string) => {
+    const filteredCalendar = calendar.filter((item) => item.id != id);
+    setCalendar([...filteredCalendar]);
+  };
+
+  const handleClick = (id: string, amount: number) => {
+    let selectedItemId = calendar.findIndex((item) =>
+      findCalendarItem(item, id)
+    );
+    if (selectedItemId === -1) {
+      if (amount > 0) addItem(id, amount);
+    } else {
+      let updatedQuantity = calendar[selectedItemId].quantity + amount;
+      if (updatedQuantity <= 0) {
+        removeItem(id);
+      } else {
+        setCalendar((prevState) => {
+          const newState = [...prevState];
+          newState[selectedItemId].quantity = updatedQuantity;
+          return newState;
+        });
+      }
+    }
+  };
+
   return (
     <div>
-      {/* <Categories callback={handleTypeClick} /> */}
-      {fooddata.map((x) => {
+      <span>currently logged in as: {userList[0].name}</span>
+      <h2>Food 2 today:</h2>
+      {calendar.map((calendarItem) => {
+        const foodItemId = fooddata.findIndex(
+          (item) => item.ID === calendarItem.id
+        );
+        const foodItem = fooddata[foodItemId];
+        return (
+          <div key={foodItem.ID}>
+            <h1>{foodItem.ITEM}</h1>
+            <span>{calendarItem.quantity}</span>
+          </div>
+        );
+      })}
+      <br />
+      <br />
+      <br />
+      {fooddata.map((item) => {
         return (
           <FoodCard
-            key={x.ID}
-            item={x.ITEM}
-            id={x.ID}
-            cal={x.CAL}
-            cat={x.CATEGORY}
-            price={x.PRICE}
+            key={item.ID}
+            item={item}
+            addCalendar={handleClick}
+            disableCheck={disableCheck}
           />
         );
       })}
@@ -52,43 +135,35 @@ function App() {
   );
 }
 
-type Props = {
-  item: string;
-  cal: string;
-  cat: string;
-  price: string;
-  id: string;
-};
+interface FoodCardProps {
+  item: FoodItem;
+  addCalendar: (id: string, number: number) => void;
+  disableCheck: (id: string) => boolean | undefined;
+}
 
-const FoodCard: React.FC<Props> = ({ item, cal, cat, price, id }) => {
+const FoodCard: React.FC<FoodCardProps> = ({
+  item,
+  addCalendar: addCalendar,
+  disableCheck: disableCheck,
+}) => {
   return (
     <div>
       <hr />
       <br />
-      <span>{cal}</span>
+      <span>{item.ITEM}</span>
       <br />
-      <span>{price}</span>
-      <h1>{item}</h1>
-      <span>{cat}</span>
-      {/* adds itemtodaylist */}
-      <button onClick={() => console.log(`item ${item}, +1`)}>plus 1</button>
-      {/* subtracts itemtodaylist */}
-      <button onClick={() => console.log(`item ${item}, -1`)}>minus 1</button>
+      <button onClick={() => addCalendar(item.ID, 1)}>plus 1</button>
+      <button
+        onClick={() => addCalendar(item.ID, -1)}
+        disabled={disableCheck(item.ID)}
+      >
+        minus 1
+      </button>
+      {/* <button onClick={() => } */}
+
       <hr />
     </div>
   );
 };
-
-// const Categories: React.FC<ButtonProps> = ({ callback }) => {
-//   return (
-//     <>
-//       {CATEGORIES.map((x: any) => (
-//         <h1 key={x} onClick={() => callback(x)}>
-//           {x}
-//         </h1>
-//       ))}
-//     </>
-//   );
-// };
 
 export default App;
