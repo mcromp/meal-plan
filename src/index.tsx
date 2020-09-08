c7cc8f53098431d426c705dce1848f5ae9ee6e6d
Matthew Crompton <crompton.ml@gmail.com>
2020-09-08 10:17:50 +0200

    Cleanup index

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 import { store } from "./redux";
 import App from "./components/App";
-import "./index.css";
+import './index.css'
 
 ReactDOM.render(
   <React.StrictMode>
