433de6f5f2f30917a5d773581b006a69cdf83abc
Matthew Crompton <crompton.ml@gmail.com>
2020-09-28 13:03:48 +0200

    Fix broken url

--- src/redux/apiUrl.ts
+++ src/redux/apiUrl.ts
@@ -4,5 +4,5 @@ export const CALENDAR_URL = API_URL + "calendar/";
 export const CALENDAR_URL_ADD = CALENDAR_URL + "add/";
 
 export const USER_URL: string = API_URL + "users/";
-export const USER_MENU: string = API_URL + "menu/";
-export const USER_URL_SIGNUP: string = API_URL + "signup/";
+export const USER_MENU: string = USER_URL + "menu/";
+export const USER_URL_SIGNUP: string = USER_URL + "signup/";
