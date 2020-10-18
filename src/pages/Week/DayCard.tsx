7065c056f0292eb20de6d266bc48b3dfa71e1079
Matthew Crompton <crompton.ml@gmail.com>
2020-10-18 15:16:22 +0200

    Add stylings

--- src/pages/Week/DayCard.tsx
+++ src/pages/Week/DayCard.tsx
@@ -1,21 +1,23 @@
-import React from "react"
+import React, { useState } from "react"
 import { useSelector } from "react-redux"
-import { Link } from "react-router-dom"
+import { Redirect } from "react-router-dom"
 import { RootState } from "../../redux"
 import { WeekDay, CalendarItem, MenuItemJSON } from "../../shared/types"
 import './styles/week.css'
 
 
 const DayCard: React.FC<DayCardProps> = ({ day, calendarDisplay }) => {
+  const [isClicked, setIsClicked] = useState<boolean>(false);
   const menuList = useSelector<RootState, MenuItemJSON[]>(state => state.menuList)
+
+  if (isClicked) { return <Redirect to={`/d/${day.dateId}`} /> }
   return (
-    <div className="day-card">
+    <div className="day-card" onClick={() => setIsClicked(true)}>
       <div className="header">
         <h3>{day.day}</h3>
         <h4>{day.dateId}</h4>
-        <Link to={`/d/${day.dateId}`}><button>✎</button></Link>
+        <button>✎</button>
       </div>
-
       <div>
         {calendarDisplay?.menuItems.map(item => {
           const menuItem = menuList.find(f => f.ID === item.foodId)
@@ -26,7 +28,6 @@ const DayCard: React.FC<DayCardProps> = ({ day, calendarDisplay }) => {
           )
         })}
       </div>
-
     </div>
   )
 }
