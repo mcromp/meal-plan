9780412c8b7cab2ebe6e8df55a3c5e75ab649799
Matthew Crompton <crompton.ml@gmail.com>
2020-10-26 17:22:03 +0100

    Remove medbutton

--- src/shared/MedButton.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const MedButton: React.FC<any> = ({ disabled = false, onClick, children }) => {
-  return (
-    <button className="med" onClick={onClick} disabled={disabled}>{children}</button>
-  )
-}
-
-export default MedButton
