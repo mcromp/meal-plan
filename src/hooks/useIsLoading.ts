7d319c70181de604df695b44595a74f6aa9c5863
Matthew Crompton <crompton.ml@gmail.com>
2020-11-02 17:07:49 +0100

    Remove unused imports

--- src/hooks/useIsLoading.ts
+++ src/hooks/useIsLoading.ts
@@ -1,7 +1,6 @@
-import React, { useEffect, useState } from 'react'
+import { useEffect, useState } from 'react'
 import { useSelector } from 'react-redux';
 import { RootState } from '../redux';
-import Loading from '../shared/Loading';
 
 const useIsLoading = () => {
   const [isShown, setIsShown] = useState(false)
