d194653c7623a6d8c9ad864ca47937a35539e655
Matthew Crompton <crompton.ml@gmail.com>
2020-11-06 10:38:39 +0100

    Add AddFoodItemProps type

--- src/pages/EditMenu/AddFoodItem.tsx
+++ src/pages/EditMenu/AddFoodItem.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-const AddFoodItem: React.FC<any> = ({ setIsAddModalShown }) => {
+const AddFoodItem: React.FC<AddFoodItemProps> = ({ setIsAddModalShown }) => {
   return (
     <div className="food-item add-food-item" onClick={() => setIsAddModalShown(true)}>
       <span className="food-item__title add-food-item__title">Add your own menu item!</span>
@@ -9,5 +9,8 @@ const AddFoodItem: React.FC<any> = ({ setIsAddModalShown }) => {
   )
 };
 
+type AddFoodItemProps = {
+  setIsAddModalShown: (bool: boolean) => void;
+}
 
 export default AddFoodItem;
