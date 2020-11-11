22537328d575739b7aa2889e43cccf726f92ae10
Matthew Crompton <crompton.ml@gmail.com>
2020-11-11 20:50:48 +0100

    update url

--- src/redux/fetchHelper/apiURL.ts
+++ src/redux/fetchHelper/apiURL.ts
@@ -1,16 +1,15 @@
- export const API_URL: string = "https://ancient-fortress-30675.herokuapp.com/";
+export const API_URL: string = "https://ancient-fortress-30675.herokuapp.com/";
 
- export const CALENDAR_URL = API_URL + "calendar/";
- export const CALENDAR_URL_UPDATE = CALENDAR_URL + "update/";
- export const CALENDAR_URL_GETMANY = CALENDAR_URL + "user/";
+export const CALENDAR_URL = API_URL + "calendar/";
+export const CALENDAR_URL_UPDATE = CALENDAR_URL + "update/";
+export const CALENDAR_URL_GETMANY = CALENDAR_URL + "user/";
 
- export const MENU_URL = API_URL + "menu/";
- export const MENU_URL_ADD = MENU_URL + "add/"
- 
+export const MENU_URL = API_URL + "menu/";
+export const MENU_URL_ADD = MENU_URL + "add/";
 
- export const USER_URL: string = API_URL + "users/";
- export const USER_URL_SIGNUP: string = USER_URL + "signup/";
+export const USER_URL: string = API_URL + "users/";
+export const USER_URL_SIGNUP: string = USER_URL + "signup/";
 
- export const FAV_ADD: string = USER_URL + "favAdd/";
- export const FAV_REMOVE: string = USER_URL + "favRemove/";
- export const FAV_CLEAR: string = USER_URL + "favClear/";
+export const FAV_ADD: string = USER_URL + "favAdd/";
+export const FAV_REMOVE: string = USER_URL + "favRemove/";
+export const FAV_CLEAR: string = USER_URL + "favClear/";
