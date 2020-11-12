e833f054553eae189f6c9f5542016aae9769a28f
Matthew Crompton <crompton.ml@gmail.com>
2020-11-12 15:10:21 +0100

    Init testing

---
+++ src/__test__/Header.test.tsx
@@ -0,0 +1,19 @@
+// import * as React from 'react';
+// import Header from '../pages/Header/Header'
+// //@ts-ignore
+// import { render, RenderResult } from '@testing-library/react';
+// import { Provider } from 'react-redux'
+// import mockState from './mock/mockState';
+// let documentBody: RenderResult;
+// describe('<Header />', () => {
+//   beforeEach(() => {
+//     documentBody = render(
+//       <Provider store={mockState}>
+//         <Header />
+//       </Provider>);
+//   });
+//   it('shows not found message', () => {
+//     expect(documentBody.getBytext('Hello This is for test purposes')).toBeInTheDocument();
+//   });
+// });
+export const x = () => { };
