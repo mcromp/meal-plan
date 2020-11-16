b66b321891aef3bac04b1d714c25e7a3e0f36c8a
Matthew Crompton <crompton.ml@gmail.com>
2020-11-16 12:24:40 +0100

    Shorten timeout for useisloading

--- src/hooks/useIsLoading.ts
+++ src/hooks/useIsLoading.ts
@@ -1,22 +1,21 @@
-import { useEffect, useState } from 'react'
-import { useSelector } from 'react-redux';
-import { RootState } from '../redux';
+import { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
+import { RootState } from "../redux";
 
 const useIsLoading = () => {
-  const [isShown, setIsShown] = useState(false)
-  const isLoading = useSelector<RootState, boolean>(state => state.isLoading);
+ const [isShown, setIsShown] = useState(false);
+ const isLoading = useSelector<RootState, boolean>((state) => state.isLoading);
 
-  useEffect(() => {
-    let timeout = setTimeout(() => {
-      setIsShown(isLoading)
-    }, 750);
-    return () => {
-      clearTimeout(timeout)
-    }
-  }, [isLoading]);
+ useEffect(() => {
+  let timeout = setTimeout(() => {
+   setIsShown(isLoading);
+  }, 550);
+  return () => {
+   clearTimeout(timeout);
+  };
+ }, [isLoading]);
 
-  return [isShown];
+ return [isShown];
 };
-
 
 export default useIsLoading;
